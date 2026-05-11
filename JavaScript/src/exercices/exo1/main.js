39631161ccb5eac7869f2d2babd4e733cb03dc9b
rafael-vergnes <rafael.vergnes@hotmail.com>
2026-05-11 16:42:59 +0200

    NEW : dossier js

---
+++ JavaScript/src/exercices/exo1/main.js
@@ -0,0 +1,30 @@
+console.log("Hello World JS");
+
+// Changer la couleur du titre
+let titrePage = document.querySelector("h1");
+titrePage.style.color = "skyblue";
+
+
+
+let prenom = ["Rafaël", "Julien"];
+console.log(prenom)
+
+let nombre = [1,2,3];
+console.log(nombre)
+
+let nombre_virgule = [3.75,2.5,1.25];
+console.log(nombre_virgule)
+
+let moi = {color:"Violet", age:"26", height:"1m73"};
+console.log(moi)
+
+let bonjour = function () {
+    console.log("Hello World !")
+}
+
+let calcul = (7530.25 + 5279.42 - 2257.57) / 1558.35 * 4.83 ** 3
+console.log(calcul)
+
+let compteur = 0
+compteur ++
+console.log(compteur)
