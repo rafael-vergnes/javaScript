d955e97675f93fafd6a6d9b96938a3a6167337ad
rafael-vergnes <rafael.vergnes@hotmail.com>
2026-05-12 15:35:24 +0200

    NEW : exo4

---
+++ JavaScript/src/exercices/exo4/main.js
@@ -0,0 +1,14 @@
+function trente_trois(a) {
+    return 33 + a
+}
+
+const resultat_1 = trente_trois(17)
+console.log(resultat_1)
+
+
+function addition(a, b) {
+    return a + b
+}
+
+const resultat_2 = addition(3, 7)
+console.log(resultat_2)
