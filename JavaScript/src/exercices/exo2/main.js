fd15c37254cc1521d28e512a1edc3b6ebc80a4fb
rafael-vergnes <rafael.vergnes@hotmail.com>
2026-05-12 12:26:47 +0200

    NEW : exo3 array

---
+++ JavaScript/src/exercices/exo2/main.js
@@ -0,0 +1,13 @@
+let pizzas = ["margarita", 12.90, "peperroni", 14.90, "reine", 13.90, "hawaïenne", 14.90, "forestière", 15.90];
+console.log(pizzas)
+
+let nom = ["Rafaël"];
+let adresse = ["25 boulevard des champs élysés 75000 Paris"];
+let heure = ["19h30"];
+let message = `Merci ` + nom + ` d'avoir commandé chez "La Pizzeria Raffinata". Votre commande arrivera à ` + heure + ` à l'adresse suivante : ` + adresse + `.
+    Qu'est-ce qu'une pizza dit à une autre pizza qui lui fait de l'ombre ? 
+    "Arrête de me trancher la route !"`
+console.log(message)
+
+const date = new Date();
+console.log(date);
---
+++ JavaScript/src/exercices/exo3/main.js
@@ -0,0 +1,9 @@
+let prenom = ["Adrian"]
+let age = [16]
+let passions = ["tennis", "jeux-vidéo"]
+
+let tabUser = [prenom, age, passions]
+console.log(tabUser[2])
+console.log(tabUser[2][1])
+
+
